fda8133fe96c129ac7715e248d01957151b82370
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-09 09:04:57 +0300

    pending

--- script.js
+++ script.js
@@ -17,5 +17,37 @@ for(let items of shoppingList){
 }
 buttonEvent.addEventListener('click',inputToArray())
 }
+// BEGIN ANOTHER SCOPE CHAIN TO make the code work.
 
-console.log(buttonClickEvent())
+
+// Accessing the Addbutton, creating a callback fucntion and attaching an event listener.
+// accessing the addButton element.
+const addButton=document.getElementById('addButton')
+// creating a callback function,second argument for the addEventListener.
+function AddsItemsFromInputFieldFromHtml(){
+    // code for adding elements from the input field of Html.
+}
+// attaching an event lsitener
+addButton.addEventListener('click',)
+
+
+// marking bought items to show that they have been bought.we can yse a check list
+// and add an event listener.
+function markItemInListAsBought(){
+    // write code here
+}
+// attaching an eventListener
+.addEventListener('click')
+// how do i add a checkList to mark as tick when an item is bought
+
+
+
+// Accessing the clearList id, creating a callback fucntion and attaching an event listener. 
+// attach an event listener to clear the list of items.
+const clearButton =document.getElementById('clearList')
+// creating a callback function.
+function clearListOfItems(){
+// enter code here to clear list.
+}
+// attaching an event listener.
+clearButton.addEventListener('click',)
